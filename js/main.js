23301cc8e7dd7eb11e2ea4371f8d842b98ddb8d4
nancy nguyen <prancynguyen@gmail.com>
2017-11-14 17:25:16 -0800

    Created general user scheme

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 console.log('main.js loaded!');
 
 $(document).ready(function() {
-  console.log('document loaded!');
+  console.log('document loaded');
+  $template = document.querySelector('#template');
+  console.log($template)
+  $anchor = document.querySelector('#anchor');
+  let template = _.template($template.innerHTML);
+  $anchor.innerHTML = template({content: fancyData});
 });
+
